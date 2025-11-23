d1a840de2b825a728327426b6c20df23f564ed37
Pechdanai-Saepong <nineza321@gmail.com>
2025-11-23 13:49:06 +0700

    Update mogodb connection

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ConcertModule } from './concert/concert.module';
 import { UserModule } from './user/user.module';
 import { ReserveModule } from './reserve/reserve.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://pechdanaisp:bCgfM9C95jMXQ5Ev@cluster0.mv8urii.mongodb.net/?appName=Cluster0'),
+    ConfigModule.forRoot({ isGlobal: true }), // load .env
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: `mongodb+srv://${config.get('MONGO_USER')}:${config.get('MONGO_PASS')}@${config.get('MONGO_HOST')}/?appName=${config.get('MONGO_DB')}`,
+      }),
+    }),
     ConcertModule,
     ReserveModule,
-    UserModule
+    UserModule,
   ],
   controllers: [],
   providers: [],
